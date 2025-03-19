6e3d4c5f6b66d601734ef51881e3e3ba19818988
prajwalnaveeenn <139122004+prajwalnaveeenn@users.noreply.github.com>
2025-03-19 21:33:49 +0530

    Added files

---
+++ src/main/java/db/MyConnection.java
@@ -0,0 +1,34 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+
+    public static Connection getConnection(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ytproject?useSSL=false", "root", "PouseyLova69");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Connection is done");
+        return connection;
+    }
+
+    public static void closeConnection(){
+        if(connection != null){
+            try{
+                connection.close();
+            } catch (SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        MyConnection.getConnection();
+//    }
+}
